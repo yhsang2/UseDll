74ab04f5a4f97605808aaa73d1cb9d2d3f527bc9
HAN SANGHOON <yhsang2@naver.com>
2021-05-20 13:21:10 +0900

    Add files via upload

---
+++ Test_UseDLL/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Test_CreateDLL;
+
+namespace Test_UseDLL
+{
+    public partial class Form1 : Form
+    {
+        dllTest var_dll = new dllTest();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(var_dll.fn_Add(5, 10).ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
